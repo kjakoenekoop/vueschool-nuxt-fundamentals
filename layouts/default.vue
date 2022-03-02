bdda65fe7a3913d517ff46b069ab7d617180c9f7
kjakoenekoop <k.j.a.koenekoop@gmail.com>
2022-03-02 15:55:06 +0700

    finished course

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <the-navbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "~/components/TheNavbar";
+export default {
+  components: { TheNavbar }
+}
+</script>
